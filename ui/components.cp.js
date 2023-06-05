727835c178e88092cd1e7a3d8ee9ecd31028ce7b
Robin Schreiber <robin.schreiber@me.com>
2023-06-05 17:22:56 +0200

    first commit Commited from within lively.next.

--- ui/components.cp.js
+++ ui/components.cp.js
@@ -1,1 +1,27 @@
-'format esm';
+import { component, ViewModel } from 'lively.morphic';
+import { pt } from 'lively.graphics/geometry-2d.js';
+import { Color } from 'lively.graphics/color.js';
+
+class BattutaModel extends ViewModel {
+  get bindings () {
+    return [
+      { target: 'button', signal: 'onMouseDown', handler: 'action' }
+    ];
+  }
+
+  action () {
+    this.ui.button.fill = Color.random(0, 100);
+  }
+}
+
+const IbnBatutta = component({
+  extent: pt(101.7, 132.9),
+  defaultViewModel: BattutaModel,
+  submorphs: [{
+    name: 'button',
+    borderColor: Color.rgb(23, 160, 251),
+    borderWidth: 1,
+    extent: pt(38.3, 9.7),
+    position: pt(42.3, 42.1)
+  }]
+});
